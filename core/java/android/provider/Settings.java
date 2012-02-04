2a223d689580532c387e1b30dc9226166f615ad0
Davor Bertovic <davor@losinj.com>
2012-02-04 23:07:34 +0100

    Link/unlink volume/notification volume setting (framework part)

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2200,6 +2200,13 @@ public final class Settings {
         public static final String VOLUME_WAKE_SCREEN = "volume_wake_screen";
 
 
+	/**
+	 * Link/unlink volume/notification volume
+	 *
+	 * @hide
+	 */
+	public static final String VOLUME_LINK_NOTIFICATION = "volume_link_notification";
+
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
