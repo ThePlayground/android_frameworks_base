8e6c113e370b7f3d7c3c8f7ca81c15a00ac42140
Arun Kumar K.R <akkr@codeaurora.org>
2012-03-04 01:19:39 +0100

    libsurfaceflinger: Add support for WFD
    - In enableHDMIOutput, decide the type of external display to enable.
    - HDMI has preference over WIFI Display, handleEventHDMI decides the
    priority.

--- services/surfaceflinger/SurfaceFlinger.h
+++ services/surfaceflinger/SurfaceFlinger.h
@@ -184,7 +184,7 @@ public:
 
 #ifdef QCOM_HDMI_OUT
     //HDMI Specific
-    virtual void                        enableHDMIOutput(int enable);
+    virtual void                        enableHDMIOutput(int externaltype);
     virtual void                        setActionSafeWidthRatio(float asWidthRatio);
     virtual void                        setActionSafeHeightRatio(float asHeightRatio);
 #endif
@@ -363,7 +363,7 @@ private:
 
 #ifdef QCOM_HDMI_OUT
             //HDMI Specific
-            void updateHwcHDMI(bool enable);
+            void updateHwcHDMI(int externaltype);
 #endif
 #ifdef QCOM_HARDWARE
             bool isGPULayerPresent();
@@ -423,7 +423,7 @@ private:
 
 #ifdef QCOM_HDMI_OUT
                 //HDMI specific
-                bool                        mHDMIOutput;
+                int                         mHDMIOutput;
                 Mutex                       mHDMILock;
                 bool                        mOrientationChanged;
 #endif
