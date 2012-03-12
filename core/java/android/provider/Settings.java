454f3fcca754539a7cb4229244adb71453bd8bc0
TwistedUmbrella <twistedumbrella@gmail.com>
2012-03-12 19:57:39 -0400

    frameworks/base: Dual-pane options part (2/2)

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -1958,7 +1958,14 @@ public final class Settings {
          * @hide
          */
         public static final String NAVIGATION_BAR_VISIBLE = "navigation_bar_visible";
-        
+
+        /**
+         * Whether the soft keys should display if device already has hard keys. The
+         * value is boolean (1 or 0).
+         * @hide
+         */
+        public static final String DUAL_PANE_SETTINGS = "dual_pane_settings";
+
         /**
          * Show pointer location on screen?
          * 0 = no
