351c5640abfd2aabfa7a03979dee5d66da8d9fa7
TwistedUmbrella <twistedumbrella@gmail.com>
2012-03-13 00:44:31 -0400

    MemoryHeapBase: Save and binderize the offset

--- libs/binder/Android.mk
+++ libs/binder/Android.mk
@@ -31,10 +31,13 @@ sources := \
     ProcessState.cpp \
     Static.cpp
 
+ifneq ($(TARGET_SENSE_CAMERA),true)
+    GLOBAL_CFLAGS += -DBINDERIZE_OFFSET=1
+endif
+
 ifeq ($(TARGET_USES_ION),true)
     sources += MemoryHeapIon.cpp
 endif
-
 
 LOCAL_PATH:= $(call my-dir)
 
