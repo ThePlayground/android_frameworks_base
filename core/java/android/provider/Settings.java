5da8334ced3095055487427e20f4bba058f6c32b
nebkat <nebkat@teamhacksung.org>
2012-03-17 11:25:22 +0000

    Fix custom LED values string
    Change-Id: I8c44ec327f752932d9200142a620d577b6850804

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -1950,7 +1950,7 @@ public final class Settings {
          * Which custom LED values to use for the notification pulse LED.
          * @hide
          */
-        public static final String NOTIFICATION_LIGHT_PULSE_CUSTOM_VALUES = "notification_light_pulse_custom_enable";
+        public static final String NOTIFICATION_LIGHT_PULSE_CUSTOM_VALUES = "notification_light_pulse_custom_values";
 
         /**
          * Whether the soft keys should display if device already has hard keys. The
