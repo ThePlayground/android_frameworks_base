b741ba1269e1fb173f21c35645538ba79e8d8e12
Tejas Shikhare <tshikhar@codeaurora.org>
2012-03-28 16:11:40 -0700

    libstagefright: Change thread priority for LPA threads
    - Since A2DP behaves like a render thread, there is a need for it
      run at urgent audio priority.
    
    (cherry picked from commit 3b81741adf7b743cfa72874f63bf561950c9cd22)
    
    Change-Id: I9d7ee924766fef1ac77c47dc445d8d32a305d700

--- media/libstagefright/LPAPlayerALSA.cpp
+++ media/libstagefright/LPAPlayerALSA.cpp
@@ -791,7 +791,8 @@ void LPAPlayer::decoderThreadEntry() {
 
     pthread_mutex_lock(&decoder_mutex);
 
-    setpriority(PRIO_PROCESS, 0, ANDROID_PRIORITY_AUDIO);
+    pid_t tid  = gettid();
+    androidSetThreadPriority(tid, ANDROID_PRIORITY_AUDIO);
     prctl(PR_SET_NAME, (unsigned long)"LPA DecodeThread", 0, 0, 0);
 
     LOGV("decoderThreadEntry wait for signal \n");
@@ -933,7 +934,8 @@ void LPAPlayer::eventThreadEntry() {
     int avail = 0;
     int i = 0;
 
-    setpriority(PRIO_PROCESS, 0, ANDROID_PRIORITY_AUDIO);
+    pid_t tid  = gettid();
+    androidSetThreadPriority(tid, ANDROID_PRIORITY_AUDIO);
     prctl(PR_SET_NAME, (unsigned long)"LPA EventThread", 0, 0, 0);
 
 
@@ -1060,7 +1062,8 @@ void *LPAPlayer::A2DPThreadWrapper(void *me) {
 }
 
 void LPAPlayer::A2DPThreadEntry() {
-    setpriority(PRIO_PROCESS, 0, ANDROID_PRIORITY_AUDIO);
+    pid_t tid  = gettid();
+    androidSetThreadPriority(tid,ANDROID_PRIORITY_URGENT_AUDIO);
     prctl(PR_SET_NAME, (unsigned long)"LPA A2DPThread", 0, 0, 0);
 
     while (1) {
