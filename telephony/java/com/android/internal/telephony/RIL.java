bd084d1a9c241d9ad627cdb80610575ad1b4b07b
Synergy Dev <synergycomputing@gmail.com>
2012-05-09 12:19:04 -0700

    Bring inline with Intervigilium RIL class

--- telephony/java/com/android/internal/telephony/RIL.java
+++ telephony/java/com/android/internal/telephony/RIL.java
@@ -2053,7 +2053,14 @@ public class RIL extends BaseCommands implements CommandsInterface {
         switch(stateInt) {
             case 0: state = RadioState.RADIO_OFF; break;
             case 1: state = RadioState.RADIO_UNAVAILABLE; break;
-            case 10: state = RadioState.RADIO_ON; break;
+            case 2: state = RadioState.SIM_NOT_READY; break;
+            case 3: state = RadioState.SIM_LOCKED_OR_ABSENT; break;
+            case 4: state = RadioState.SIM_READY; break;
+            case 5: state = RadioState.RUIM_NOT_READY; break;
+            case 6: state = RadioState.RUIM_READY; break;
+            case 7: state = RadioState.RUIM_LOCKED_OR_ABSENT; break;
+            case 8: state = RadioState.NV_NOT_READY; break;
+            case 9: state = RadioState.NV_READY; break;
 
             default:
                 throw new RuntimeException(
@@ -2937,6 +2944,7 @@ public class RIL extends BaseCommands implements CommandsInterface {
 
         return response;
     }
+
 
     protected Object
      responseICC_IO(Parcel p) {
